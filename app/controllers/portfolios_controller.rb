2579b4c4e4943afc0104157335c1c346db7cca27
Siyu An <siyuan@Siyus-MacBook-Air.local>
2017-10-03 21:54:42 -0400

    portfolioDone

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,57 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolios = Portfolio.all
+  end
+
+  def new
+    @portfolios = Portfolio.new
+  end
+
+  def create
+    @portfolios = Portfolio.new(params.require(:portfolio).permit(:title, :body))
+
+    respond_to do |format|
+      if @portfolios.save
+        format.html { redirect_to @portfolio, notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolios }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolios.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  	@portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+  	@portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(params.require(:portfolio).permit(:title, :body))
+        format.html { redirect_to @portfolio, notice: 'Portfolio was successfully updated.' }
+        format.json { render :show, status: :created, location: @portfolios }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolios.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def destroy
+  	@portfolio = Portfolio.find(params[:id])
+    @portfolio.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+  
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "#{blog} blog post",
+    body: "snajn hluihuih ndakjwnbwhql j;qjinjbqkbn2bm "
+  )
+end
+puts "10 blogs created"
+
+5.times do |skill|
+  Skill.create!(
+  	title: "#{skill}",
+    percentage: 15
+  )
+end
+puts "5 skills created"
+
+9.times do |portfolio|
+  Portfolio.create!(
+  	title: "#{portfolio} title",
+  	body: "asddasdasdas",
+  	main_image: "http://via.placeholder.com/600x400",
+  	thumb_image: "http://via.placeholder.com/600x400"
+  )
+end
+
+puts "9 portfolio created"
