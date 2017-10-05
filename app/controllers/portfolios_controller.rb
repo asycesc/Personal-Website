ec5d4a43a3d8b3c8c66151300fb99812f97460aa
Siyu An <siyuan@dhcp-v880-18-00654.1Xwireless.unc.edu>
2017-10-05 16:49:41 -0400

    refactor portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,40 +1,39 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
-    @portfolios = Portfolio.all
+    @portfolio = Portfolio.all
   end
 
   def angular
-    @portfolios = Portfolio.angular
+    @portfolio = Portfolio.angular
   end
 
   def new
-    @portfolios = Portfolio.new
-    3.times {@portfolios.technologies.build}
+    @portfolio = Portfolio.new
+    3.times {@portfolio.technologies.build}
   end
 
   def create
-    @portfolios = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
-
+    @portfolio = Portfolio.new(portfolio_params)
     respond_to do |format|
-      if @portfolios.save
-        format.html { redirect_to portfolio_show_path(@portfolios.id), notice: 'Portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolios }
+      if @portfolio.save
+        format.html { redirect_to portfolio_show_path(@portfolio.id), notice: 'Portfolio was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio }
       else
         format.html { render :new }
-        format.json { render json: @portfolios.errors, status: :unprocessable_entity }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def edit
-  	@portfolio = Portfolio.find(params[:id])
   end
 
   def update
-  	@portfolio = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to @portfolio, notice: 'Portfolio was successfully updated.' }
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolio_show_path(@portfolio.id), notice: 'Portfolio was successfully updated.' }
         format.json { render :show, status: :created, location: @portfolios }
       else
         format.html { render :new }
@@ -44,18 +43,26 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio = Portfolio.find(params[:id])
   end
 
   def destroy
-  	@portfolio = Portfolio.find(params[:id])
     @portfolio.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
-  
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+    end
 
 
 
