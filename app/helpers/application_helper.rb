b35e49f44effb7df176fd278781307e3fe583513
Siyu An <siyuan@Siyus-MacBook-Air.local>
2017-10-05 22:44:18 -0400

    view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def login_condition
+		if current_user.is_a?(User)
+			link_to "Logout", destroy_user_session_path, method: :delete
+		else
+			(link_to "SignUp", new_user_registration_path, method: :get) +
+			"<br>".html_safe +
+			(link_to "Login", new_user_session_path, method: :get)
+		end
+	end
 end
