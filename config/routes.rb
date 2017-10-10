ca54888cf828a10d8c7c2ec2b35953bf1566d1c5
Siyu An <siyuan@dhcp-v880-20-00745.1Xwireless.unc.edu>
2017-10-10 16:08:14 -0400

    topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
+	resources :topics, only: [:index, :show]
+
 	devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
 	resources :portfolios  do
 		put :sort, on: :collection
 	end
 
 	get 'angular_items', to: 'portfolios#angular'
-
 	get 'about', to: 'pages#about'
-
 	get 'contact', to: 'pages#contact'
 	get 'news', to: 'pages#tech_news'
 
