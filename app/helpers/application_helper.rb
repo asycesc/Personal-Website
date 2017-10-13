beca8ea82be8fb3cee174e58ff1c5f42ecc7756a
Siyu An <siyuan@Siyus-MacBook-Air.local>
2017-10-12 21:27:25 -0400

    implement markdown in portfolio show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -69,6 +69,24 @@ module ApplicationHelper
 		end
 	end
 
+	def markdown text
+		coderayified = CodeRayify.new(filter_html: true, hard_warp: true)
+		extensions = {
+			fenced_code_blocks: true,
+			no_intra_emphasis: true,
+			auto_links: true,
+			lax_html_blocks: true
+		}
+		markdown_html = Redcarpet::Markdown.new(coderayified, extensions)
+		markdown_html.render(text).html_safe
+	end
+
+	class CodeRayify < Redcarpet::Render::HTML
+		def block_code(code, language)
+			CodeRay.scan(code, language).div
+		end
+	end
+
 	def alert_generator msg
 		js add_gritter(msg,  sticky: false)
 	end
--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,24 +3,6 @@ module BlogsHelper
 		image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
 	end
 
-	def markdown text
-		coderayified = CodeRayify.new(filter_html: true, hard_warp: true)
-		extensions = {
-			fenced_code_blocks: true,
-			no_intra_emphasis: true,
-			auto_links: true,
-			lax_html_blocks: true
-		}
-		markdown_html = Redcarpet::Markdown.new(coderayified, extensions)
-		markdown_html.render(text).html_safe
-	end
-
-	class CodeRayify < Redcarpet::Render::HTML
-		def block_code(code, language)
-			CodeRay.scan(code, language).div
-		end
-	end
-
 	def blog_status_color blog
 		if blog.draft?
 			'color: red;'
